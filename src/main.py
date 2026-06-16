78c33da8ea99884f886746309a967af7ad075dd5
Meighanr <rorymeighan99@gmail.com>
2026-06-16 13:53:23 +0100

    Updated main code

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(0, 10, 100)
+x = np.linspace(0, 10, 10000)
 y = np.sin(x)
 
 print("environment is working")
+print("Test 2")
 print(x[:5])
 
 plt.plot(x, y)
 plt.title("Test Plot")
-plt.show()
+plt.show() 
